af8c2961f03bc2d45bd6477f3d7cc73c036668f4
kunalsuri <kunalsuri@outlook.com>
2024-07-24 10:51:45 +0200

    Update version_finder.py

--- utils/version_finder.py
+++ utils/version_finder.py
@@ -1,6 +1,21 @@
 import numpy
 import torch
 
+# Numpy
+# To find the version of the Numpy installed in your system
+
 print("Numpy Version is : " + numpy.__version__)
 
+# PyTorch
+# To find the version of the PyTorch installed in your system
+# Latest version of the PyTorch can be downloaded via : https://pytorch.org/
+
 print("Torch Version is : " + torch.__version__)
+
+# CUDA
+# Check if CUDA is availabe | To verify if the correct version of the PyTorch has been installed
+
+print(torch.cuda.is_available())
+
+x = torch.rand(5, 3)
+print(x)
