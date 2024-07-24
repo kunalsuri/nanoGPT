0647cdc3ec0292c2cbed2c29bcf9c740764f3023
kunalsuri <kunalsuri@outlook.com>
2024-07-24 22:24:02 +0200

    created utilities

--- utils/version_finder.py
+++ utils/version_finder.py
@@ -14,6 +14,7 @@ print("Torch Version is : " + torch.__version__)
 
 # CUDA
 # Check if CUDA is availabe | To verify if the correct version of the PyTorch has been installed
+# CUDA 2.3.1 RC : https://dev-discuss.pytorch.org/t/pytorch-release-2-3-1-final-rc-is-available/2126
 
 print("Torch Version for CUDA available : ") 
 print(torch.cuda.is_available())
